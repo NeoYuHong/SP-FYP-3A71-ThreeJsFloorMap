8005619ad26923e10e707554ca3b4486101c1438
Yuhong <103405186+NeoYuHong@users.noreply.github.com>
2024-11-30 22:37:55 +0800

    add popup when hover over store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { MapControls } from 'three/addons/controls/MapControls.js';
 import * as THREE from 'three';
 import floor1 from './floor1.glb';
 import * as dat from 'dat.gui';
+import storeData from './data.json'
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 var stats = new Stats();
+const popup = document.getElementById("popup");
+document.body.appendChild(stats.dom);
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -61,10 +64,13 @@ scene.add(grid);
 const loader = new GLTFLoader();
 var floorScene = null;
 loader.load(floor1, function (gltf) {
+
     scene.add(gltf.scene);
     console.log(gltf.scene)
     floorScene = gltf.scene;
     console.log("added")
+
+
 }, undefined, function (error) {
     console.error(error);
 });
@@ -85,11 +91,22 @@ var gui = new dat.GUI();
 
 // Data folder
 var data = {
-    hovering: "none"
+    hovering: "none",
+    storeName: 'nil',
+    storeDesc: 'nil',
+    clientX: 0,
+    clientY: 0
 }
 var guiData = gui.addFolder('Data');
-var controller = guiData.add(data, 'hovering', 0, 3).name('Hovering over:').listen();
-controller.domElement.querySelector('input').disabled = true; // Make the controller read-only
+
+const hoveringDataGUI = guiData.add(data, 'hovering', 0, 3).name('Hovering over:').listen();
+const storeNameGUI = guiData.add(data, 'storeName').name('Store name:').listen();
+const storeDescGUI = guiData.add(data, 'storeDesc').name('Store desc:').listen();
+
+hoveringDataGUI.domElement.querySelector('input').disabled = true;
+storeNameGUI.domElement.querySelector('input').disabled = true;
+storeDescGUI.domElement.querySelector('input').disabled = true;
+
 guiData.open();
 
 
@@ -142,6 +159,10 @@ document.onmousemove = function (e) {
     mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
 }
 
+document.addEventListener("mousemove", (event) => {
+    data.clientX = event.clientX
+    data.clientY = event.clientY
+})
 
 function onHover(item) {
 
@@ -151,20 +172,44 @@ function onHover(item) {
     raycaster.setFromCamera(mouse, camera);
     intersects = raycaster.intersectObject(item);
 
+
+
     if (intersects.length !== 0) {
 
-        data.hovering = intersects[0].object.name;
-
         if (prevObj && prevColor && intersects[0].object != prevObj) {
-
             prevObj.material.color.set(prevColor);
-
-
             prevColor = null;
             prevObj = null;
         }
 
-        let obj = intersects[0].object;
+        let obj = intersects.find((obj) => obj.object.userData.parent)?.object
+
+        if (!obj) {
+            if (intersects[0].object.userData.id)
+
+                data.hovering = intersects[0].object.userData.id;
+            else
+                data.hovering = intersects[0].object.name;
+            data.storeName = 'nil'
+            popup.style.display = 'none'
+            return
+        }
+
+        data.hovering = obj.userData.name;
+
+        let test = storeData.find(({ unit_number }) => unit_number == obj.userData.name)
+        data.storeName = test?.stores[0].store_name || 'nil'
+        data.storeDesc = test?.stores[0].store_description || 'nil'
+        // Set popup content
+        popup.innerText = `Unit: ${obj.userData.name}\nStore name: ${test?.stores[0].store_name || 'nil'}\nStore desc: ${test?.stores[0].store_description || 'nil'}`;
+
+        // Set popup position and show it
+        popup.style.display = 'block'
+        popup.style.left = `${data.clientX + 10}px`;
+        popup.style.top = `${data.clientY + 10}px`;
+        popup.style.display = "block";
+
+        // let obj = intersects[0].object;
 
         if (!prevColor)
             prevColor = new THREE.Color(obj.material.color);
@@ -181,7 +226,11 @@ function onHover(item) {
         if (prevObj && prevColor)
             prevObj.material.color.set(prevColor.getHex());
 
+        popup.style.display = 'none'
+
         data.hovering = "none";
+        data.storeName = 'nil'
+        data.storeDesc = 'nil'
 
         prevObj = null;
         prevColor = null;
