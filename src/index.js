835d2774862fa85d590ab2adb0ecfd423730ca4d
Yuhong <103405186+NeoYuHong@users.noreply.github.com>
2024-11-30 12:02:48 +0800

    add limits to control and change to map control

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { MapControls } from 'three/addons/controls/MapControls.js';
 import * as THREE from 'three';
 import floor1 from './floor1.glb';
 import * as dat from 'dat.gui';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,28 +18,52 @@ scene.add(light);
 const light2 = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
 scene.add(light2);
 
-var controls = new OrbitControls(camera, renderer.domElement);
+const controls = new MapControls(camera, renderer.domElement);
+
+// var controls = new OrbitControls(camera, renderer.domElement);
 // controls.minDistance = 2;
 // controls.maxDistance = 5;
+
+controls.maxPolarAngle = (Math.PI / 2) * 0.75;
+controls.minPolarAngle = (Math.PI / 2) * 0.4;
+
 controls.target.set(10, 0, 4);
+controls.maxDistance = 25;
+controls.minDistance = 5.5;
+controls.zoomSpeed = 1.6;
+
 camera.position.set(13, 5, 10);
 camera.rotation.set(-1.1, 0, 0);
+
 controls.update();
 
 
+const baseBorderSize = 25;
+const minPan = new THREE.Vector3(-baseBorderSize, 0, -baseBorderSize);
+const maxPan = new THREE.Vector3(baseBorderSize, 0, baseBorderSize);
+var _v = new THREE.Vector3();
+
+controls.addEventListener("change", function () {
+    _v.copy(controls.target);
+    controls.target.clamp(minPan, maxPan);
+    _v.sub(controls.target);
+    camera.position.sub(_v);
+})
+
+let grid = new THREE.GridHelper(baseBorderSize * 2, 100, "white", "white");
+grid.position.y = 0.01;
+scene.add(grid);
+
+// Load the model
 const loader = new GLTFLoader();
 var floorScene = null;
 loader.load(floor1, function (gltf) {
-
     scene.add(gltf.scene);
     console.log(gltf.scene)
     floorScene = gltf.scene;
     console.log("added")
-
 }, undefined, function (error) {
-
     console.error(error);
-
 });
 
 var raycaster = new THREE.Raycaster();
