4566ef8cdf1ba62893a57a52606a49441e06481f
Yuhong <103405186+NeoYuHong@users.noreply.github.com>
2024-11-30 12:03:09 +0800

    add 2D and border size to gui

--- src/index.js
+++ src/index.js
@@ -73,26 +73,72 @@ var intersects = [];
 var prevObj = null;
 var prevColor = null;
 
+
+
+
+
+// Create gui for debug
+var gui = new dat.GUI();
+
+// Data folder
 var data = {
     hovering: "none"
 }
+var guiData = gui.addFolder('Data');
+var controller = guiData.add(data, 'hovering', 0, 3).name('Hovering over:').listen();
+controller.domElement.querySelector('input').disabled = true; // Make the controller read-only
+guiData.open();
 
 
-var gui = new dat.GUI();
 
-var box = gui.addFolder('Data');
+// Settings folder
+var params = {
+    is2D: false,
+    showGrid: true,
+    borderSize: baseBorderSize
+};
+var guiSettings = gui.addFolder('Settings');
+guiSettings.add(params, "is2D").name("2D").onChange(function (value) {
+    if (value) {
+        controls.maxPolarAngle = (Math.PI / 2) * 0;
+        controls.minPolarAngle = (Math.PI / 2) * 0;
+        controls.maxDistance = 50;
+        controls.minDistance = 5.5;
+        controls.update();
+    } else {
+        controls.maxPolarAngle = (Math.PI / 2) * 0.75;
+        controls.minPolarAngle = (Math.PI / 2) * 0.4;
+        controls.maxDistance = 25;
+        controls.minDistance = 5.5;
+        controls.update();
+    }
+})
+guiSettings.add(params, "showGrid").name("Show Grid").onChange(function (value) {
+    grid.visible = value;
+})
+guiSettings.add(params, "borderSize", 20, 100).name("Border Size").onChange(function (value) {
+    minPan.set(-value, 0, -value);
+    maxPan.set(value, 0, value);
+    _v.copy(controls.target);
+    controls.target.clamp(minPan, maxPan);
+    _v.sub(controls.target);
+    camera.position.sub(_v);
 
-var controller = box.add(data, 'hovering', 0, 3).name('Hovering over:').listen();
+    // properly scale grid to match size
+    grid.position.set(0, 0, 0);
+    grid.scale.set(value / baseBorderSize, 1, value / baseBorderSize);
 
-// Make the controller read-only
-controller.domElement.querySelector('input').disabled = true;
 
-box.open();
+})
+guiSettings.open();
+
+gui.open();
 
 document.onmousemove = function (e) {
     mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
     mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
 }
+
 
 function onHover(item) {
 
@@ -139,6 +185,8 @@ function onHover(item) {
 
     }
 }
+
+
 
 function animate(frame) {
     requestAnimationFrame(animate);
